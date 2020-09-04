54886e78deb24eb84bdad0ca05bcb6ad3b933484
양정훈 <luciferkala@gmail.com>
2020-09-05 07:09:13 +0900

    change winston module to class and adjust singleton pattern

---
+++ test/functionalTest.test.ts
@@ -0,0 +1,8 @@
+import express, {Request, Response} from "express"
+import request from "supertest"
+import {LogService} from "../config/winston"
+import {app} from "../src/app";
+
+describe('functional test', () => {
+	
+});
