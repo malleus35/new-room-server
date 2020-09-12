8adb355b4dd9264a24ca17206d45199648c11109
양정훈 <luciferkala@gmail.com>
2020-09-12 21:17:21 +0900

    make all jwt method test

--- test/middlewares/jwt.test.ts
+++ test/middlewares/jwt.test.ts
@@ -6,18 +6,45 @@ const logger = LogService.getInstance();
 
 env.chooseEnv(env.envTestConfig);
 describe("json webtoken test", () => {
-    it("basic json webtoken sign test", () => {
-        let mJwt: any;
-        jest.spyOn(jwt, "sign").mockResolvedValueOnce(mJwt);
-        const createToken = jwt.sign(
-            { foo: "bar" },
-            process.env.JWT_SECRET_KEY,
-            {
-                algorithm: "RS256",
-                expiresIn: "1h"
-            }
-        );
-        console.log(createToken);
-        expect(jwt.sign).toBeCalledTimes(1);
+    it("Test basic json webtoken sign", () => {
+        const mJwtSign = jest.spyOn(jwt, "sign");
+        const payload = { foo: "bar" };
+        const secretKey = process.env.JWT_SECRET_KEY;
+        const options = {
+            expiresIn: "1h"
+        };
+        let createToken = jwt.sign(payload, secretKey, options);
+        logger.info(createToken);
+        expect(mJwtSign).toBeCalledTimes(1);
+        expect(mJwtSign).toBeCalledWith(payload, secretKey, options);
+    });
+
+    it("Test jwt verify method", () => {
+        const mJwtVerify = jest.spyOn(jwt, "verify");
+        const payload = { foo: "bar" };
+        const secretKey = process.env.JWT_SECRET_KEY;
+        const options = {
+            expiresIn: "1h"
+        };
+        let createToken = jwt.sign(payload, secretKey, options);
+        const decoded = jwt.verify(createToken, secretKey);
+        logger.info(decoded.foo);
+        expect(decoded.foo).toEqual("bar");
+        expect(mJwtVerify).toBeCalledTimes(1);
+        expect(mJwtVerify).toBeCalledWith(createToken, secretKey);
+    });
+
+    it("Test jwt decode method", () => {
+        const payload = { foo: "bar" };
+        const secretKey = process.env.JWT_SECRET_KEY;
+        const options = {
+            expiresIn: "1h"
+        };
+        let createToken = jwt.sign(payload, secretKey, options);
+        const mJwtDecode = jest.spyOn(jwt, "decode");
+        let decoded = jwt.decode(createToken);
+        expect(mJwtDecode).toBeCalledTimes(1);
+        expect(mJwtDecode).toBeCalledWith(createToken);
+        expect(decoded.foo).toEqual("bar");
     });
 });
