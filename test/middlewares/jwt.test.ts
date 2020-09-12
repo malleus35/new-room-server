1805b34a520e76c0aff787664e28e7fed6971e3a
양정훈 <luciferkala@gmail.com>
2020-09-12 11:33:45 +0900

    make jwt sign method test

--- test/middlewares/jwt.test.ts
+++ test/middlewares/jwt.test.ts
@@ -1,7 +1,23 @@
-import jsonwebtoken from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
+import env from "@src/custom/dotenv";
+import LogService from "@src/custom/LogService";
+const logger = LogService.getInstance();
+
+env.chooseEnv(env.envTestConfig);
 describe("json webtoken test", () => {
     it("basic json webtoken sign test", () => {
-        expect(1).toBe(1);
+        let mJwt: any;
+        jest.spyOn(jwt, "sign").mockResolvedValueOnce(mJwt);
+        const createToken = jwt.sign(
+            { foo: "bar" },
+            process.env.JWT_SECRET_KEY,
+            {
+                algorithm: "RS256",
+                expiresIn: "1h"
+            }
+        );
+        console.log(createToken);
+        expect(jwt.sign).toBeCalledTimes(1);
     });
 });
