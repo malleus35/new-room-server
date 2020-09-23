b526e594e603e52b1ed74808c35dae0396f1e344
양정훈 <luciferkala@gmail.com>
2020-09-23 15:50:33 +0900

    Fix webpack error install pg module to install webpack-node-externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-const path = require('path');
-const webpack = require('webpack');
-const Dotenv = require('dotenv-webpack')
+const path = require("path");
+const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
+const nodeExternals = require("webpack-node-externals");
 module.exports = {
     mode: "production",
     target: "node",
@@ -16,27 +17,28 @@ module.exports = {
             "@src": path.resolve(__dirname, "src"),
             "@env": path.resolve(__dirname, "envs"),
             "@log": path.resolve(__dirname, "log")
-
         }
     },
     module: {
-        rules: [{
-            test: /\.(ts|tsx)$/,
-            exclude: [/node_modules/, /test/],
-            use: {
-                loader: "babel-loader",
-                options: {
-                    presets: [
-                        "@babel/preset-env",
-                        "@babel/preset-typescript"
-                    ],
-                    plugins: [
-                        "@babel/proposal-class-properties",
-                        "@babel/proposal-object-rest-spread"
-                    ]
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: [/node_modules/, /test/],
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-typescript"
+                        ],
+                        plugins: [
+                            "@babel/proposal-class-properties",
+                            "@babel/proposal-object-rest-spread"
+                        ]
+                    }
                 }
             }
-        }]
+        ]
     },
     plugins: [
         new Dotenv({
@@ -44,5 +46,6 @@ module.exports = {
             safe: true,
             silent: true // load .env.example (defaults to "false" which does not use dotenv-safe)
         })
-    ]
+    ],
+    externals: [nodeExternals()]
 };
