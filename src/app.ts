211b621f1e242b1b82a75520a8a9600bd64a4936
junghun yang <luciferkala@gmail.com>
2020-11-17 20:01:52 +0900

    add InitController

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import env from "@src/utils/Dotenv";
 import LogService from "@src/utils/LogService";
 import apiRouter from "@src/api/index";
+import InitController from "@src/controllers/services/InitController";
 
 env.chooseEnv();
 const app = express();
@@ -20,6 +21,7 @@ app.use(
         stream: { write: (message) => LogService.getInstance().info(message) }
     })
 );
+const init = new InitController().excute()();
 app.use("/api", apiRouter);
 if (process.env.NODE_ENV !== "test")
     app.listen(process.env.SERVER_PORT || 3000);
