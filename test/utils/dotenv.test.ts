ec9346368a45caa9bf8622315d7c059bae001bfe
junghun yang <luciferkala@gmail.com>
2021-02-05 07:59:16 +0900

    delete legacy test code

--- test/utils/dotenv.test.ts
+++ test/utils/dotenv.test.ts
@@ -4,6 +4,6 @@ describe("Test dotenv test", () => {
         env.chooseEnv();
         expect(process.env.SERVER_PORT).not.toEqual("5000");
         expect(process.env.SERVER_PORT).not.toEqual("8080");
-        expect(process.env.SERVER_PORT).toEqual("8001");
+        expect(process.env.SERVER_PORT).toEqual("8000");
     });
 });
